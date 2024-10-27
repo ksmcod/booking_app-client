4d3e809a64536600403d6c404b95a26416d664cf
Kono Steve <stevekono77@gmail.com>
2024-10-27 17:16:44 +0100

    added Hotel type in types.ts file

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,21 @@ export interface User {
   email: string;
   image?: string;
 }
+
+export interface Hotel {
+  id: string;
+  name: string;
+  city: string;
+  country: string;
+  description: string;
+  type: string;
+  adultCount: number;
+  childrenCount: number;
+  facilities: string[];
+  price: number;
+  starRating: number;
+  imageUrls: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  userId: string;
+}
