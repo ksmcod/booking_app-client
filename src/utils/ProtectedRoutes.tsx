f2c1e149322a8b1a1afbb1872909df0edc5fc8a3
Kono Steve <stevekono77@gmail.com>
2024-10-29 05:22:39 +0100

    fixed error in 'ProtectedRoutes' component

--- src/utils/ProtectedRoutes.tsx
+++ src/utils/ProtectedRoutes.tsx
@@ -1,8 +1,14 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useAppSelector } from "@/app/hooks";
 
 export default function ProtectedRoutes() {
+  const navigate = useNavigate();
   const isLoggedIn = useAppSelector((state) => state.user.isLoggedIn);
 
-  return isLoggedIn ? <Outlet /> : Navigate({ to: "login", replace: true });
+  if (isLoggedIn) {
+    return <Outlet />;
+  } else {
+    // return <div className="">{navigate("/login", { replace: true })}</div>;
+    navigate("/login", { replace: true });
+  }
 }
