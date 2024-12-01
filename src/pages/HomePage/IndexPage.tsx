6639aa9f9cc7980d20b8960c9e7c24c31788f491
Kono Steve <stevekono77@gmail.com>
2024-12-01 14:28:20 +0100

    feat: completed index page

--- src/pages/HomePage/IndexPage.tsx
+++ src/pages/HomePage/IndexPage.tsx
@@ -3,7 +3,9 @@ import HotelCard from "@/components/HotelCard";
 import Loader from "@/components/Loader";
 
 export default function Index() {
-  const { data: hotels, isLoading } = useGetHotelsQuery("hotest");
+  const { data: hotHotels, isLoading: loading1 } = useGetHotelsQuery("hotest");
+  const { data: recentHotels, isLoading: loading2 } =
+    useGetHotelsQuery("recent");
 
   // if (isLoading) {
   //   return (
@@ -13,10 +15,7 @@ export default function Index() {
   //   );
   // }
 
-  console.log("Hotels are: ", hotels);
-
-  if (hotels === undefined && !isLoading) {
-    // if (!isLoading) {
+  if ((!hotHotels && !loading1) || (!recentHotels && !loading2)) {
     return (
       <div className="flex-1 flex flex-col justify-center items-center">
         <h1 className="text-xl font-bold">An error occured</h1>
@@ -25,26 +24,45 @@ export default function Index() {
         </p>
       </div>
     );
-    // }
   }
 
   return (
-    <div className="space-y-4">
-      <h1 className="text-4xl font-bold">Hotest destinations</h1>
+    <div className="space-y-10">
+      <div className="space-y-5">
+        <h2 className="text-4xl font-bold">Most recent additions</h2>
 
-      {isLoading ? (
-        <div className="flex justify-center">
-          <Loader className="size-14" />
-        </div>
-      ) : (
-        <>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-            {hotels?.map((hotel) => (
-              <HotelCard hotel={hotel} />
-            ))}
+        {loading2 ? (
+          <div className="flex justify-center">
+            <Loader className="size-14" />
           </div>
-        </>
-      )}
+        ) : (
+          <>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
+              {recentHotels?.map((hotel) => (
+                <HotelCard hotel={hotel} />
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+
+      <div className="space-y-5">
+        <h2 className="text-4xl font-bold">Hotest destinations</h2>
+
+        {loading1 ? (
+          <div className="flex justify-center">
+            <Loader className="size-14" />
+          </div>
+        ) : (
+          <>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+              {hotHotels?.map((hotel) => (
+                <HotelCard hotel={hotel} />
+              ))}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
